b725ba8347055b551f69b208ec8660c9dd96a22f
Budgerigar295 <samardude67@gmail.com>
2025-03-28 14:45:02 -0400

    The Start

---
+++ Lightweight Image Conversion/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Lightweight_Image_Conversion
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
